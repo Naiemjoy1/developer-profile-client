cbb7c420cbedcec1339db55b3ecb221caca44e1e
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-05 00:43:36 +0600

    nav link added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        openSans: "'Open Sans', sans-serif ",
+        Playfair: "'Playfair Display', serif",
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#389afe",
+          secondary: "#fefefe",
+          accent: "#d4d8dd",
+          neutral: "#f1f2f5",
+        },
+      },
+      {
+        dark: {
+          primary: "#1972c8",
+          secondary: "#111825",
+          accent: "#616975",
+          neutral: "#131313",
+        },
+      },
+    ],
+  },
 };
