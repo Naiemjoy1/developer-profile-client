9a75748a1f1ec2e59bc6e57f307f125e3452cccc
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-08 13:42:08 +0600

    project update

--- src/Pages/Home/Projects/ProjectCard.jsx
+++ src/Pages/Home/Projects/ProjectCard.jsx
@@ -1,44 +1,47 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 const ProjectCard = ({ project }) => {
   return (
     <div className="transition-all duration-500 m-1 rounded-lg group relative hover:scale-[1.10] cursor-pointer">
-      <Link to={project.links.live} target="_blank">
-        <div className="bg-secondary shadow-2xl rounded-xl text-center py-6 px-6 space-y-4 transition-all duration-500">
-          <p className="text-sm">{project.name}</p>
-          <div className="h-32 flex justify-center">
-            <img
-              src={project.img}
-              className="h-full w-auto rounded-lg"
-              alt={project.name}
-            />
-          </div>
-          <p className="text-sm text-left">{`${project.description.slice(
-            0,
-            100
-          )}...`}</p>
-          <div className="overflow-hidden">
-            <ul className="flex flex-wrap gap-2 max-w-full">
-              <li className="text-sm font-semibold">Tools:</li>
-              {project.tools.map((tool, toolIndex) => (
-                <li
-                  className="text-sm whitespace-nowrap overflow-hidden overflow-ellipsis"
-                  key={toolIndex}
-                >
-                  &quot;{tool}&quot;
-                </li>
-              ))}
-            </ul>
-          </div>
-          <section className="text-left text-sm flex gap-2">
-            <p className="font-semibold">Links:</p>
-            <a href={project.links.live}>Live</a>
-            <a href={project.links.client}>client</a>
-            <a href={project.links.server}>server</a>
-          </section>
+      <div className="bg-secondary shadow-2xl rounded-xl text-center py-6 px-6 space-y-4 transition-all duration-500">
+        <p className="text-sm">{project.name}</p>
+        <div className="h-32 flex justify-center">
+          <img
+            src={project.img}
+            className="h-full w-auto rounded-lg"
+            alt={project.name}
+          />
         </div>
-      </Link>
+        <p className="text-sm text-left">{`${project.description.slice(
+          0,
+          100
+        )}...`}</p>
+        <div className="overflow-hidden">
+          <ul className="flex flex-wrap gap-2 max-w-full">
+            <li className="text-sm font-semibold">Tools:</li>
+            {project.tools.map((tool, toolIndex) => (
+              <li
+                className="text-sm whitespace-nowrap overflow-hidden overflow-ellipsis"
+                key={toolIndex}
+              >
+                &quot;{tool}&quot;
+              </li>
+            ))}
+          </ul>
+        </div>
+        <section className="text-left text-sm flex gap-2">
+          <p className="font-semibold">Links:</p>
+          <a href={project.links.live} target="_blank">
+            Live
+          </a>
+          <a href={project.links.client} target="_blank">
+            client
+          </a>
+          <a href={project.links.server} target="_blank">
+            server
+          </a>
+        </section>
+      </div>
     </div>
   );
 };
